422ccda51b3e41a615c4783d506ddbd77083d5de
Wilson Delgado <wilsondelgadomoreno@gmai.com>
2024-11-11 18:31:54 -0500

    Inializacion de Java Varibales y conceptos

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // VARIABLES
+        /*
+        */
+        int edad = 31;
+        double precio = 300.40;
+        boolean disponible = true; //false/true
+        char genero;
+        genero = 'M'; // M = Masculino / F = Femenino
+
+        // Aceder a las varibles para mandarlas a imprimir
+        System.out.println(edad);
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+        edad = 35; // se sobre escriben los valores
+    }
+}
